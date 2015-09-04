189a4a6134ac947c1451cf321d77fe0b7a863394
SeongJunLim <dla123kr@gmail.com>
2015-09-04 09:42:01 +0900

    Modify OleDB.cs
    - Delete Category, Menu

--- client/Once_v2_2015/Once_v2_2015/Class/OleDB.cs
+++ client/Once_v2_2015/Once_v2_2015/Class/OleDB.cs
@@ -51,31 +51,14 @@ namespace Once_v2_2015.Class
                 try
                 {
                     conn.Open();
-
-                    // 카테고리 정보
-                    cmd.CommandText =
-                        "CREATE TABLE MENU_CATEGORY([MENU_CATEGORY_NUM] IDENTITY PRIMARY KEY, [MENU_CATEGORY_NAME] VARCHAR, [MENU_CATEGORY_DELETE] CHAR DEFAULT N)";
-                    cmd.ExecuteNonQuery();
-                    // 메뉴 정보 (외래키 : MENU_CATEGORY_NUM)
-                    cmd.CommandText =
-                        "CREATE TABLE MENU([MENU_NUM] IDENTITY PRIMARY KEY, [MENU_NAME] VARCHAR, [MENU_SIZE] CHAR, [MENU_TEMP] CHAR, [MENU_WHIP] CHAR DEFAULT N, [MENU_PRICE] INT, [MENU_DELETE] CHAR DEFAULT N, [MENU_CATEGORY_NUM] INT)";
-                    cmd.ExecuteNonQuery();
-                    cmd.CommandText =
-                        "ALTER TABLE MENU ADD CONSTRAINT MENU_CATEGORY_FK FOREIGN KEY (MENU_CATEGORY_NUM) REFERENCES MENU_CATEGORY(MENU_CATEGORY_NUM)";
-                    cmd.ExecuteNonQuery();
+                    
                     // 결제 정보
                     cmd.CommandText =
                         "CREATE TABLE RECEIPT([RECEIPT_NUM] identity primary key, [RECEIPT_DATE] DATETIME, [RECEIPT_TYPE] char, [RECEIPT_DISCOUNT] int DEFAULT 0, [RECEIPT_SUBTOTAL] int, [RECEIPT_AMOUNT] int)";
                     cmd.ExecuteNonQuery();
-                    // 판매 (메뉴) 정보 (외래키 : MENU_NUM, RECEIPT_NUM)
+                    // 판매 (메뉴) 정보
                     cmd.CommandText =
                         "CREATE TABLE SALE([SALE_NUM] IDENTITY PRIMARY KEY, [MENU_NUM] INT, [RECEIPT_NUM] INT, [SALE_QUANTITY] INT)";
-                    cmd.ExecuteNonQuery();
-                    cmd.CommandText =
-                        "ALTER TABLE SALE ADD CONSTRAINT MENU_FK FOREIGN KEY (MENU_NUM) REFERENCES MENU(MENU_NUM)";
-                    cmd.ExecuteNonQuery();
-                    cmd.CommandText =
-                        "ALTER TABLE SALE ADD CONSTRAINT RECEIPT_FK FOREIGN KEY (RECEIPT_NUM) REFERENCES RECEIPT(RECEIPT_NUM)";
                     cmd.ExecuteNonQuery();
                 }
                 catch(Exception err)
